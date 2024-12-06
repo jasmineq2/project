0394b89aede3dbd69fcacdf631fd3714ca32a9c8
Vaibhav Sharma <88285949+vbv-shm@users.noreply.github.com>
2024-12-07 03:06:40 +0530

    Update configurations.py

--- benchmarks/configurations.py
+++ benchmarks/configurations.py
@@ -35,7 +35,7 @@ configurations = {
             "parameters": {
                 "height": 40,
                 "width": 40,
-                "homophily": 3,
+                "homophily_ratio": .3,
                 "radius": 1,
                 "density": 0.625,
             },
@@ -47,7 +47,7 @@ configurations = {
             "parameters": {
                 "height": 100,
                 "width": 100,
-                "homophily": 8,
+                "homophily_ratio": .8,
                 "radius": 2,
                 "density": 0.8,
             },
